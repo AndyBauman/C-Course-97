aa37c25e97ef0d9eebbb3177a7cecedc6fcdcfb4
AndyBauman <andybauman7@gmail.com>
2019-12-10 09:28:45 -0800

    Update Program.cs

--- C-Course-97/Program.cs
+++ C-Course-97/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,39 @@ namespace C_Course_97
     {
         static void Main(string[] args)
         {
+            //Concatenate three strings.
+
+            //Convert a string to uppercase.
+
+            Console.WriteLine("What's your name?");
+            string name = "<Type your name here>";
+            name = Convert.ToString(Console.ReadLine());
+
+            // Use the + and += operators for one-time concatenations.
+            string str1 = "Hello " + name + ".";
+            System.Console.WriteLine(str1);
+
+            str1 = " It was great to meet you " + name;
+            string str2 = str1.ToUpper();
+            //Console.WriteLine(" \n have a pleasent day \n \t See you next time.");
+            Console.WriteLine(str2 + " \n Have a pleasent day \n \t See you next time.");
+
+
+
+
+            // StringBuilder.
+
+            //StringBuilder sb = new StringBuilder("Welcome to The Tech Academy, ", 50);
+
+            //sb.Append("Here's what you'll learn: ");
+            //sb.AppendLine("Computer Programming");
+            //sb.AppendLine("And many more things");
+
+            //Console.WriteLine(sb);
+
+            Console.ReadLine();
+
         }
     }
 }
+
